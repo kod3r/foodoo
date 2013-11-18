37ef232004f3b1e7feebdd552900bb0ea04c36c3
Julian Pittleman <jnpn87@gmail.com>
2013-11-18 14:08:33 -0500

    zz

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,10 +68,8 @@ V1foodoo::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
     port: 587,
-    authentication: 'plain',
-    enable_starttls_auto: true,
-    user_name: "MANDRILL_USERNAME",
-    password: "MANDRILL_API_KEY"
+    user_name: ENV["MANDRILL_USERNAME"],
+    password: ENV["MANDRILL_API_KEY"]
   }
   config.action_mailer.default_url_options = { :host => 'http://foodoo.io' }
 

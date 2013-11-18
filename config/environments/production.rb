fb5320de0894b2a8a328713be603491f0fd94520
Julian Pittleman <jnpn87@gmail.com>
2013-11-18 16:19:50 -0500

    z

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,6 @@ V1foodoo::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.mandrillapp.com",
     port: 587,
-    track_clicks: false,
     user_name: ENV["MANDRILL_USERNAME"],
     password: ENV["MANDRILL_API_KEY"]
   }
